8d9bb6e46c6867810b9b8217441c6bacd0e46ab5
Francoi054 <fr.vel54@gmail.com>
2022-06-01 13:59:21 +0200

    maj

--- index.php
+++ index.php
@@ -24,16 +24,63 @@ if(isset($_POST['menu'])){
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr"></html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/style.css">
-    <link rel="stylesheet" href="./CSS/style_contact.css">
-    <script src="https://kit.fontawesome.com/84861a5cc6.js" crossorigin="anonymous"></script>
-    <title>Accueil Burger Company</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>BURGER Company</title>
 </head>
+
+<body>
+    
+    <header>
+        <nav>
+            <a class="navalt" href="./index.php"> Accueil </a>
+            <a class="navalt" href="./index.php"> Inscription </a>
+            <a class="navalt" href="./index.php"> Connexion </a>
+            <a class="navalt" href="./contact.php"> Contact </a>
+            <a class="navalt" href="./faq.php"> F.A.Q </a>
+        </nav>
+    </header>
+
+    <aside>
+        <!-- <div class="icones">
+            <img src="./Images/cheeseburger-g0006d9a87_1280.png">
+            <img src="./Images/icones/burger-au-fromage.png">
+            <img src="./Images/icones/boisson-non-alcoolisee.png">
+            <img src="./Images/icones/bol-a-salade-dessine-a-la-main.png">
+            <img src="./Images/icones/dessert.png">
+        </div> -->
+        <div>
+            <img class="logo" src="./Images/LOGO_BURGER_mofif.png">
+        </div>
+
+        <nav class="nav">
+            <a class="navalt" href="#">MENUS</a> <br>
+            <a class="navalt"href="#">BURGERS</a><br>
+            <a class="navalt"href="#"> BOISSONS</a><br>
+            <a class="navalt"href="#">SALADES</a><br>
+            <a class="navalt"href="#">DESSERTS</a> 
+        </nav>
+
+        <h3 class="copy"> &copy 2022</h3>
+        <a class="mention" href="#"> Mentions Légales </a>
+    </aside>
+    
+    <main>
+        <h1>Burger Company</h1>
+        <h2>Le meilleur dans votre assiette !</h2>
+    </main>
+
+
+    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+   
+</body>
+</html>
 
 <?php
     // if(isset($_GET['page'])){
@@ -49,45 +96,3 @@ if(isset($_POST['menu'])){
     //     } 
     // }
 ?>
-
-<body class="container">
-    <header>
-        <div class="img">
-            <img src="./Images/LOGO_BURGER_mofif.png">
-        </div>
-        <div class="titre">
-            <h1><span> BURGER Company</span></h1>
-            <h3> Pour vous servir</h3>
-        </div>
-        <nav>
-            <a href="./Index.php"> Accueil </a>
-            <a href="./contact.php"> Contact </a>
-            <a href="./faq.php"> F.A.Q </a>
-        </nav>
-    </header>
-    <main></main>
-    <aside>
-        <div class="icones">
-            <img src="./Images/menu_burger.png" alt="">
-            <img src="./Images/burger.png" alt="">
-            <img src="./Images/soda.png" alt="">
-            <img src="./Images/salad.png" alt="">
-            <img src="./Images/cupcake.png" alt="">
-        </div>
-        <nav class="nav">
-            <a href="#">MENUS</a> <br>
-            <a href="#">BURGERS</a><br>
-            <a href="#"> BOISSONS</a><br>
-            <a href="#">SALADES</a><br>
-            <a href="#">DESSERTS</a> 
-        </nav>
-    </aside>
-    <footer>
-        <h3> &copy 2022</h3>
-        <a href="#"> Mentions Légales </a>
-    </footer>
-    
-</body>
-
-
-</html>
