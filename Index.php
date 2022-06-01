2df0d97d01874136e43e95387f70a16a5b8dd4ae
fatiha54 <fatiha.hammar@laposte.net>
2022-06-01 10:19:06 +0200

    update

---
+++ Index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./CSS/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/fb7bc50dd3.js" crossorigin="anonymous"></script>
+
+    <title>BURGER Company</title>
+</head>
+<body>
+    <main>
+        <aside>
+            <div class="top">
+                <img class="logo" src="./Images/LOGO_BURGER_mofif.png" >
+            </div>
+            
+            <div class="social">
+                <div class="icon">
+                    <i class="fa-brands fa-twitter"></i><!--lien vers icone fontawsome-->
+                </div>
+                <div class="icon">
+                    <i class="fa-brands fa-facebook"></i>
+                </div>
+                <div class="icon">
+                    <i class="fa-brands fa-instagram"></i>
+                </div>
+                <div class="icon">
+                    <i class="fa-brands fa-skype"></i>
+                </div>
+                <div class="icon">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </div>
+            </div>
+            
+            <nav class="nav">
+                <!-- <div class="left">
+                    <img src="./Images/cheeseburger-g0006d9a87_1280.png">
+                    <img src="./Images/icones/burger-au-fromage.png">
+                    <img src="./Images/icones/boisson-non-alcoolisee.png">
+                    <img src="./Images/icones/bol-a-salade-dessine-a-la-main.png">
+                    <img src="./Images/icones/dessert.png">
+                </div> -->
+                <div class="right">
+                    <a class="link" href="#">MENU</a>
+                    <a class="link" href="#">BURGER</a>
+                    <a class="link" href="#">BOISSON</a>
+                    <a class="link" href="#">SALADE</a>
+                    <a class="link" href="#">DESSERT</a>
+                </div>
+
+            </nav>
+            
+            <div class="foot">
+                <h5>© Copyright 2022</h5>
+                <h6>Designed by <span>Vf</span></h6><!--span pour colorer un bout de texte-->
+            </div>
+
+        </aside>
+
+        <section>
+            <h1>BURGER Company</h1>
+            <h3 class="ligne">Le meilleur dans votre assiette</h3>
+            <!-- <img class="resto" src="./Images/backgroung_menu.jpg" alt=""> -->
+            <hr>
+
+        </section>
+
+        
+    </main>
+    
+</body>
+
+    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+   
+</body>
+</html>
