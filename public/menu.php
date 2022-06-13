94d2f248a849cc40a7b7647c2cb1955d7a974b13
fatiha54 <106582338+fatiha54@users.noreply.github.com>
2022-06-13 09:08:46 +0200

    ajout fichier Inscr

--- public/menu.php
+++ public/menu.php
@@ -1,14 +1,14 @@
 <main>
     <div class="menu_burger">
-        <img src="/images/cheeseburger-g0006d9a87_1280.png">
+        <img src="./images/cheeseburger-g0006d9a87_1280.png">
        <h2> BURGER AU CHOIX </h2> 
     </div>
     <div class="menu_frites">
-        <img src="/images/Produits/french-fries-g89a9fb918_1920.jpg">
+        <img src="./images/Produits/french-fries-g89a9fb918_1920.jpg">
         <h2> Nos accompagnements</h2>
     </div>
     <div class="menu_boisson">
-        <img src="/Images/BOISSONS/froides/Sprite.jpg">
+        <img src="./Images/BOISSONS/froides/Sprite.jpg">
         <h2> Goutez à nos délicieuses boissons</h2>
     </div>
 </main>
